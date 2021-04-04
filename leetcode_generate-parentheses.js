98382889848777c32c03c0184c726115409a436f
kyomin <kim031504@naver.com>
2021-04-05 00:16:17 +0900

    릿코드 - Generate Parentheses : DFS, 재귀

---
+++ leetcode_generate-parentheses.js
@@ -0,0 +1,25 @@
+var generateParenthesis = function(n) {
+    if(n===0) 
+        return [];
+    
+    let result = [];
+
+    function dfs(result, s, left, right) {
+        if(left > right)    // 짝이 안 맞는 경우이다.
+            return;
+
+        if(left === 0 && right === 0) {
+            result.push(s);
+            return;
+        }
+
+        if(left > 0)
+            dfs(result, s+'(', left-1, right);
+        if(right > 0)
+            dfs(result, s+')', left, right-1);
+    }
+
+    dfs(result, '', n, n);
+
+    return result;
+};
