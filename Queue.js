a52af6892e7c6b5efb7e3bf422e351e8a6caec35
kyomin <kim031504@naver.com>
2021-12-07 00:43:53 +0900

    자료구조 - 큐 함수로 정의

--- Queue.js
+++ Queue.js
@@ -1,25 +1,20 @@
-class Queue {           // Queue 자료구조 클래스 정의
-    constructor() {
-        this.arr = [];
-        this.length = 0;
-    }
+function Queue() {
+	this.arr = [];
 
-    push(element) {
-        this.arr.unshift(element);
-        this.length += 1;
-    }
+	this.push = (element) => {
+		this.arr.unshift(element);
+	};
 
-    pop() {
-        this.arr.pop();
-        this.length -= 1;
-    }
+	this.pop = () => {
+		this.arr.pop();
+	};
 
-    empty() {
-        if(this.arr.length === 0) return true;
-        else return false;
-    }
+	this.empty = () => {
+		if (this.arr.length === 0) return true;
+		else return false;
+	};
 
-    front() {
-        return this.arr[this.length-1];
-    }
+	this.front = () => {
+		return this.arr[this.arr.length - 1];
+	};
 }
